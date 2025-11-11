9a83eccbebce1980a3896d6f6e490001615b4e3f
Alx1307 <154526610+Alx1307@users.noreply.github.com>
2025-11-12 00:18:01 +0300

    Update settings.py

--- client/settings.py
+++ client/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'client.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'client/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
